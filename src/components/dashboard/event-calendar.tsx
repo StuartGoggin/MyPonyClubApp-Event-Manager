94429c9e0db146276727fb7d29144f512b548cb4
Stuart Goggin <stuart.goggin@gmail.com>
2025-08-11 01:40:38 +0000

    that didnt work  the day columns are still a fixed width

--- src/components/dashboard/event-calendar.tsx
+++ src/components/dashboard/event-calendar.tsx
@@ -117,12 +117,12 @@ const CalendarGrid = ({
                 return (
                     <div
                         key={day.toString()}
-                        className={cn('relative flex flex-col p-1.5 min-h-24', {
+                        className={cn('relative flex flex-col p-1.5', {
                           'text-muted-foreground': !isSameMonth(day, month),
                           'bg-muted/20': !isSameMonth(day, month) && (isSaturday || isSunday),
                           'bg-primary/5': isSameMonth(day, month) && (isSaturday || isSunday),
                           'relative': isCurrentDayToday,
-                          'p-1 min-h-20': isYearView,
+                          'p-1': isYearView,
                         })}
                     >
                          <span
@@ -415,3 +415,4 @@ export function EventCalendar({
     
 
     
+

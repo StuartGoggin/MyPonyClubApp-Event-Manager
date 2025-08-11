4112b8c2089f5802ed77bb92cfc0e5e2de9de691
Stuart Goggin <stuart.goggin@gmail.com>
2025-08-11 12:32:52 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
-import { Sheet, SheetContent } from "@/components/ui/sheet"
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet"
 import { Skeleton } from "@/components/ui/skeleton"
 import {
   Tooltip,
@@ -206,7 +206,12 @@ const Sidebar = React.forwardRef<
             }
             side={side}
           >
-            <div className="flex h-full w-full flex-col">{children}</div>
+            <div className="flex h-full w-full flex-col">
+              <SheetHeader className="p-0 text-left">
+                  <SheetTitle className="sr-only">PonyClub Events</SheetTitle>
+              </SheetHeader>
+              {children}
+            </div>
           </SheetContent>
         </Sheet>
       )
@@ -761,3 +766,5 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
